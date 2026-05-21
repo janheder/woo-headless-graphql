69422d396c7b06c41ad4f1917666385b7a8bd931
Jan Heder <info@janheder.cz>
2026-05-21 16:27:16 +0200

    Update wp-client.ts

--- src/lib/wp-client.ts
+++ src/lib/wp-client.ts
@@ -1,6 +1,7 @@
 import { createClient, cacheExchange, fetchExchange } from '@urql/core';
 
 const isServer = typeof window === 'undefined';
+const wooSessionStorageKey = 'woo-session';
 
 // Define the API endpoint dynamically based on SSR vs. Client environment
 const graphqlUrl = isServer
@@ -16,9 +17,33 @@ export const client = createClient({
   url: graphqlUrl,
   exchanges: [cacheExchange, fetchExchange],
   fetchOptions: () => {
+    const headers: Record<string, string> = {};
+
+    if (!isServer) {
+      const wooSession = window.localStorage.getItem(wooSessionStorageKey);
+
+      if (wooSession) {
+        headers['woocommerce-session'] = wooSession;
+      }
+    }
+
     return {
       // credentials: 'include' is critical for Svelte/Client queries to send WooCommerce cookies
       credentials: isServer ? 'same-origin' : 'include',
+      headers,
     };
+  },
+  fetch: async (input, init) => {
+    const response = await fetch(input, init);
+
+    if (!isServer) {
+      const wooSession = response.headers.get('woocommerce-session');
+
+      if (wooSession) {
+        window.localStorage.setItem(wooSessionStorageKey, wooSession);
+      }
+    }
+
+    return response;
   },
 });
